502524fda29fc75c45e095e6eb30e30f833f7227
Urvashi Reddy <urvashi.reddy@gmail.com>
2014-10-21 21:38:02 -0700

    create entry view for user, allow them to signin or signup

--- www/js/app.js
+++ www/js/app.js
@@ -23,24 +23,29 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('app', {
-      url: "/app",
-      abstract: true,
-      templateUrl: "templates/menu.html",
-      controller: 'AppCtrl'
+    .state('entry', {
+      url: "/entry",
+      templateUrl: "templates/entry.html",
+      controller: 'EntryCtrl'
     })
 
+    // .state('app', {
+    //   url: "/app",
+    //   abstract: true,
+    //   templateUrl: "templates/menu.html",
+    //   controller: 'AppCtrl'
+    // })
 
-    .state('app.profile', {
-      url: "/profile",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/profile.html"
-        }
-      }
-    });
+    // .state('app.profile', {
+    //   url: "/profile",
+    //   views: {
+    //     'menuContent' :{
+    //       templateUrl: "templates/profile.html"
+    //     }
+    //   })
+
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/profile');
+  $urlRouterProvider.otherwise('/entry');
 });
 
